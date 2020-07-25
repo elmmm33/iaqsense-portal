9e2581632b50f4db1b4dadc73b4a23b366ece6a4
Lesley Kwok <kwokps0920@gmail.com>
2020-07-25 09:45:15 +0800

    bugfix

--- src/pages/DevicePage/DevicePage.js
+++ src/pages/DevicePage/DevicePage.js
@@ -93,6 +93,10 @@ const DevicePage = props => {
     let telemetryApiResult = await api('get', `${envars.telemetryServiceUrl}/telemetry/${deviceId}/data${querys.join('&')}`);
     if (telemetryApiResult.data.success) {
       const results = telemetryApiResult.data.result;
+      if (results && results.length > 0) {
+        const lastDataTime = moment(results[results.length - 1].timestamp, 'YYYY-MM-DDTHH:mm:ss').add(1, 's');
+        setTelemetryLastTime(lastDataTime.valueOf()); // save the next time point;
+      }
 
       let data;
       if (!telemetryInitLoaded) {
@@ -101,8 +105,6 @@ const DevicePage = props => {
       }else{
         data = [...telemetryData];
         if (results && results.length > 0) {
-          const lastDataTime = moment(results[results.length - 1].timestamp, 'YYYY-MM-DDTHH:mm:ss').add(1, 's');
-          setTelemetryLastTime(lastDataTime.valueOf()); // save the next time point;
           results.forEach(result => { data.push(result) });
         }
       }
@@ -135,6 +137,11 @@ const DevicePage = props => {
     if (frequencyApiResult.data.success) {
       const results = frequencyApiResult.data.result;
 
+      if (results && results.length > 0) {
+        const lastDataTime = moment(results[results.length - 1].timestamp, 'YYYY-MM-DDTHH:mm:ss').add(1, 's');
+        setFrequencyLastTime(lastDataTime.valueOf()); // save the next time point;
+      }
+
       let data;
       if (!frequencyInitLoaded) {
         setFrequencyInitLoaded(true);
@@ -142,8 +149,6 @@ const DevicePage = props => {
       }else{
         data = [...frequencyData];
         if (results && results.length > 0) {
-          const lastDataTime = moment(results[results.length - 1].timestamp, 'YYYY-MM-DDTHH:mm:ss').add(1, 's');
-          setFrequencyLastTime(lastDataTime.valueOf()); // save the next time point;
           results.forEach(result => { data.push(result) });
         }
       }
